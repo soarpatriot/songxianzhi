2d681272e73ce71d767adfa29869d8e77ab6c84c
Soar <85624529@qq.com>
2015-07-20 21:19:36 +0800

    change the js

--- config/application.rb
+++ config/application.rb
@@ -36,11 +36,11 @@ module SongXianZhi
 
 
     #need fixd
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile += Ckeditor.assets
+    #  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    # config.assets.precompile += Ckeditor.assets
 
     config.assets.precompile += ['logo.js']
-    config.assets.precompile += ['i.js', 'ckeditor/*', 'html5shiv-printshiv.min.js', 'respond.min.js', 'ie10-viewport-bug-workaround.js', 'ie-emulation-modes-warning.js']
+    config.assets.precompile += [ 'html5shiv-printshiv.min.js', 'respond.min.js', 'ie10-viewport-bug-workaround.js', 'ie-emulation-modes-warning.js']
     config.assets.precompile += ['jquery.iframe-transport.js','jquery.fileupload.js','jquery.fileupload-ui.js']
 
    #config.assets.precompile += %w(error_code.css error_code.js information.css information2.css information.js information2.js iscroll.css chat.css chat.js)
