b7fe093c10570dc9cc033263d9bde05d1dad829e
Soar <85624529@qq.com>
2015-08-08 08:13:50 +0800

    remove ckeditor js

--- app/assets/javascripts/i.js
+++ app/assets/javascripts/i.js
@@ -16,9 +16,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
-//= require ckeditor/init
-//= require ckeditor/ckeditor
-//= require ckeditor_config
 //= require turbolinks
 //= require jquery.turbolinks
 //= require nprogress
