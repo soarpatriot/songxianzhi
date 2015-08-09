54fa87979a913f2bfb6e8022983de46da7eb0cf1
Soar <85624529@qq.com>
2015-08-09 12:13:12 +0800

    change container

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
      @post = Post.find(params[:id])
      @comment = Comment.new
      @comments = @post.comments.page params[:page]
+     @voters = @post.votes_for.up.by_type(User).voters
    end
 
 
