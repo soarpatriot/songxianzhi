b2037aaccdbe9b3d3539e4f4ec84d3069267b5b5
Soar <85624529@qq.com>
2015-08-17 11:54:19 +0800

    fixed wx error

--- app/assets/javascripts/wechat.js
+++ app/assets/javascripts/wechat.js
@@ -1,11 +1,11 @@
 
 $(function() {
   
-  var shareDate = {
+  var shareData = {
       imgUrl: $("#share-image-url").val(),
       title: $("#share-title").val(),
       content: $("#share-content").val(),
-      link: $("#share-link").val()
+      link: $("#share-url").val()
   };
   var appId = $("#app-id").val();
   var timestamp = $("#timestamp").val();
@@ -15,25 +15,25 @@ $(function() {
   console.log("appId"+ timestamp);
   console.log("appId" + nonceStr);
   console.log("appId" + signature);
-  if(wx){
-      wx.confg({
-        debug: true,
-        appId: appId,
-        timestamp: timestamp,
-        nonceStr:  nonceStr,
-        signature: signature,
-        jsApiList:['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone'] 
-      });
-   }
-   wx.onMenuShareTimeline({
-      title: shareData.title,
-      link: shareData.link,
-      imgUrl: shareData.imgUrl 
-   }) 
-   wx.onMenuShareAppMessage({
-      title: shareData.title,
-      desc: shareData.content,
-      link: shareData.link,
-      imgUrl: shareData.imgUrl 
-   }) 
+  wx.config({
+      debug: true,
+      appId: appId,
+      timestamp: timestamp,
+      nonceStr:  nonceStr,
+      signature: signature,
+      jsApiList:['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone'] 
+  });
+  wx.ready(function(){
+      wx.onMenuShareTimeline({
+        title: shareData.title,
+        link: shareData.link,
+        imgUrl: shareData.imgUrl 
+      }) 
+      wx.onMenuShareAppMessage({
+        title: shareData.title,
+        desc: shareData.content,
+        link: shareData.link,
+        imgUrl: shareData.imgUrl 
+      }) 
+  })
 });
